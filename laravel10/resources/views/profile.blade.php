dbfbc1912121a9eb28d4a48ee42503214e8a2f9d
Muhammad Rafky Hidayatullah <146694847+MuhammadRafkyHidayatullah@users.noreply.github.com>
2023-10-09 13:13:11 +0700

    Update Laravel

---
+++ laravel10/resources/views/profile.blade.php
@@ -0,0 +1,1 @@
+<h1>Halo, Nama saya Rafky</h1>
--- laravel10/routes/web.php
+++ laravel10/routes/web.php
@@ -16,3 +16,46 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Buat route ke halaman profile
+Route::get("/profile", function(){
+    return view("profile");
+});
+
+//route dengan parameter
+Route::get("/mahasiswa/{nama}", function($nama = "Rafky"){
+    echo "<h1>Hallo, nama Saya $nama</h1>";
+});
+
+//Route dengan paramaeter tidak wajib
+Route::get("/mahasiswa2/{nama?}", function($nama = "Rafky"){
+    echo "<h1>Hallo, Nama Saya $nama</h1>";
+});
+
+//route dengan parameter >1
+Route::get("/profile/{nama?}/{pekerjaan?}", function($nama = "Rafky", $pekerjaan = "Mahasiswa"){
+    echo "<h1>Hallo, nama Saya $nama, saya adalah seorang $pekerjaan</h1>";
+});
+
+//Redirect
+Route::get("/hubungi", function(){
+    echo "<h3>Hubungi kami </h3>";
+});
+
+Route::redirect("/contact","/hubungi");
+
+//Route Dengan nama 
+Route::get("/halo",function(){
+    echo "<a href='".route('call'). "'>".route('call')."</a>";
+});
+
+//Group
+//Memudahkan
+Route::prefix("/mahasiswa")->group(function(){
+    Route::get("/jadwal",function(){
+        echo "<h3>Jadwal Mahasiswa</h3>";
+    });
+    Route::get("/materi",function(){
+        echo "<h4>Materi Perkuliahan</h4>";
+    });
+});
